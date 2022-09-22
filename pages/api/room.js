d757f9ff6056d6d6108b58512f2f6f70f33655d7
Kitanonsan <Kitanon_san@cmu.ac.th>
2022-09-22 16:10:33 +0700

    fixed

--- pages/api/room.js
+++ pages/api/room.js
@@ -3,9 +3,9 @@ import { readDB } from "../../backendLibs/dbLib";
 export default function roomRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB().map((x) => {
-      return { roomID: x.roomId, roomName: x.roomName };
+      return { roomId: x.roomId, roomName: x.roomName };
     });
 
-    return res.status(200).json({ ok: true, rooms });
+    return res.status(200).json({ ok: true, rooms: rooms });
   }
 }
